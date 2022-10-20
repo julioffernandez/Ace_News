cb9e172ba1c3dc8ddcd1329397f8d42a1f0dbc33
Lucas <lucaslara@correo.ugr.es>
2022-10-20 18:01:38 +0200

    Settings file

--- Ace_News/settings.py
+++ Ace_News/settings.py
@@ -1,1 +1,19 @@
 # Scrapy settings for Ace_News
+
+BOT_NAME = 'Ace_News'
+
+SPIDER_MODULES = ['Ace_News.spiders']
+NEWSPIDER_MODULE = 'Ace_News.spiders'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+DOWNLOAD_DELAY = 3
+
+COOKIES_ENABLED = False
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'Ace_News.pipelines.Ace_NewsPipeline': 300,
+}
